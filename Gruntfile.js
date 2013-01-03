0f85107d46337eef88f8d9022ed157c39d493f79
Sindre Sorhus <sindresorhus@gmail.com>
2013-01-03 17:22:16 +0100

    Register jshint task and place clean task first. Fixes #4

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,8 +57,8 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['htmlmin', 'nodeunit', 'clean']);
+  grunt.registerTask('test', ['clean', 'htmlmin', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['test', 'build-contrib']);
+  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
 };
