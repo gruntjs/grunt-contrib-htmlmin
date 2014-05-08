4795655d6316123557e4bcd0aa086402ca4e1e63
Raghava Bada Chinnappa <raghava.kotekar@gmail.com>
2014-05-08 11:31:27 -0700

    Supporting directory path for source / destination to minify
    Current support of htmlmin is one-one file minification.
    
    User can’t able to give directory as source & and point to a directory
    as destination to minify all the files from source to destination

--- tasks/htmlmin.js
+++ tasks/htmlmin.js
@@ -17,7 +17,18 @@ module.exports = function (grunt) {
 
     this.files.forEach(function (file) {
       var min;
-      var src = file.src[0];
+
+      file.src.forEach(function(src){
+        var dest = file.dest;
+        if(grunt.file.isDir(dest)){
+          dest = dest + '/' + (function (fileName) {
+            if (fileName.indexOf('/') > -1) {
+              var filePathArr = fileName.split('/');
+              return filePathArr[ filePathArr.length - 1];
+            }
+            return fileName;
+          })(src);
+        }
 
       if (!grunt.file.exists(src || ' ')) {
         return grunt.log.warn('Source file "' + chalk.cyan(src) + '" not found.');
@@ -34,9 +45,10 @@ module.exports = function (grunt) {
       if (min.length < 1) {
         grunt.log.warn('Destination not written because minified HTML was empty.');
       } else {
-        grunt.file.write(file.dest, min);
-        grunt.log.writeln('Minified ' + chalk.cyan(file.dest) + ' ' + prettyBytes(max.length) + ' → ' + prettyBytes(min.length));
+        grunt.file.write(dest, min);
+        grunt.log.writeln('Minified ' + chalk.cyan(dest) + ' ' + prettyBytes(max.length) + ' → ' + prettyBytes(min.length));
       }
+      });
     });
   });
 };
